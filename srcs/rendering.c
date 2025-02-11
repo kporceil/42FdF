5910638dadc7de8c157b713e78ab12a7fffb8c3f
kporceil <163202211+Natox10@users.noreply.github.com>
2025-02-11 20:23:26 +0100

    optimizing: auto center

--- srcs/rendering.c
+++ srcs/rendering.c
@@ -14,25 +14,7 @@
 #include "map.h"
 #include <stddef.h>
 
-void	apply_translation(t_data *map_data)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (i < map_data->line)
-	{
-		j = 0;
-		while (j < map_data->col)
-		{
-			map_data->rendered_point[i][j].x += map_data->x_center;
-			map_data->rendered_point[i][j++].y += map_data->y_center;
-		}
-		++i;
-	}
-}
-
-void	apply_projection(t_data *map)
+static void	apply_projection(t_data *map)
 {
 	size_t		i;
 	size_t		j;
@@ -52,8 +34,8 @@ void	apply_projection(t_data *map)
 			rotate_x(&x, &y, &z, map);
 			rotate_y(&x, &z, map);
 			rotate_z(&x, &y, map);
-			map->rendered_point[i][j].x = x;
-			map->rendered_point[i][j].y = y;
+			map->rendered_point[i][j].x = x + map->x_center;
+			map->rendered_point[i][j].y = y + map->y_center;
 			map->rendered_point[i][j].color.rgba = map->map[i][j].color.rgba;
 			++j;
 		}
@@ -64,5 +46,4 @@ void	apply_projection(t_data *map)
 void	render_map(t_data *map_data)
 {
 	apply_projection(map_data);
-	apply_translation(map_data);
 }
